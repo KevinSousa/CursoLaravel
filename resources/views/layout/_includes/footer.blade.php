fb8a17c53ddb19e68e8a09b0f3b3548841c03a3f
unknown <kevinsmoura@hotmail.com>
2018-08-22 20:29:27 -0300

    mensagem explicando o que foi feito

---
+++ resources/views/layout/_includes/footer.blade.php
@@ -0,0 +1,15 @@
+
+     <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/js/materialize.min.js"></script>
+    <script type="text/javascript">
+    	$(document).ready(function(){
+    		Materialize.updateTextFields();
+    		$(".button-collapse").sideNav();
+    	});
+    </script>
+
+    <footer>
+       
+    </footer>
+    </body>
+  </html>
